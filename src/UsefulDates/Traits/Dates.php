616a277ee3ad98a8f4581d37b23419401d00d819
geoffreyrose <geoffrey@geoffreyrose.io>
2025-11-18 16:46:19 -0800

    move `getUsefulDatesByYear` method from `Dates` trait to `Intervals` trait

--- src/UsefulDates/Traits/Dates.php
+++ src/UsefulDates/Traits/Dates.php
@@ -6,19 +6,5 @@ use Carbon\Carbon;
 
 trait Dates
 {
-    /**
-     *  Returns Useful Dates in the specified years
-     *
-     * @param  int|null  $year  The year to get the Useful Dates in
-     */
-    public function getUsefulDatesByYear(?int $year = null): array
-    {
-        if (!$year) {
-            $year = $this->date->year;
-        }
 
-        $startOfYear = Carbon::create($year, 1, 1);
-
-        return $this->getUsefulDatesInDays($startOfYear->daysInYear() - 1, $startOfYear);
-    }
 }
